7fddf166905878a6d2f14045ae63696e8b9b87c3
andersonVick <andersonj101001@gmail.com>
2022-02-25 20:06:41 -0500

    2/25/2022 update
    fixed and added some new stuff

--- 2022r1/2022 frc 280 r/src/main/java/frc/robot/commands/index/IndexRun_CMD.java
+++ 2022r1/2022 frc 280 r/src/main/java/frc/robot/commands/index/IndexRun_CMD.java
@@ -2,25 +2,15 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.index;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Index_SUB;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+public class IndexRun_CMD extends CommandBase {
+  private final Index_SUB index;
+  public IndexRun_CMD(Index_SUB m_index) {
+    index = m_index;
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +19,9 @@ public class ExampleCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    index.SetIndexRollerspeed(.5); //FIXME The speed is subject to change.(FIX BEFORE USING)
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -38,6 +30,7 @@ public class ExampleCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    index.SetIndexRollerspeed(0);
     return false;
   }
 }
